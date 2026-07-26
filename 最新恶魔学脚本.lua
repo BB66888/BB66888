9d1f750ced8f310359b75d53edf2dcffe0d375c8
BB66888 <13292562398@163.com>
2026-07-26 21:37:22 +0800

    Add files via upload

---
+++ 最新恶魔学脚本.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kdkdirjrne/Vgxmod-Hub/refs/heads/main/Loader.lua"))()
+
+
+
+loadstring(game:HttpGet("https://fourhub13.vercel.app/Demonology.lua"))()
